bbe5f572fb735e4adc0251974431615e4b67be5f
s25abelmiku <michisillproductions@gmail.com>
2026-03-06 17:02:16 +0200

    New Functions!

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -1,54 +1,97 @@
 package datastr;
 
-public class MyArrayList {
-	private char[] list;
-	private final int DEFAULT_SIZE = 8;
-	private int size = DEFAULT_SIZE;
-	private int howManyElements = 0;
 
+public class MyArrayList {
+	//1.mainīgie
+	private char[] list;
+	private final int DEFAULT_SIZE = 8;//pec noklusjeuma bus 8 sunu masivs, ja lietotajs pats neizdomas citu skaitu
+	private int size = DEFAULT_SIZE;//8
+	private int howManyElements = 0;
+	
+	//2. konstruktori
 	public MyArrayList() {
 		list = new char[size];
-
 	}
-
+	
 	public MyArrayList(int inputSize) {
-		if (inputSize > 0) {
+		if(inputSize > 0) {
 			size = inputSize;
 		}
+				
 		list = new char[size];
 	}
-
+	
+	//3. pamatalgoritmu funkcijas
 	public boolean isEmpty() {
+		//1. garais if-else
+		/*if(howManyElements == 0) {
+			return true;//ir tukšs saraksts
+		}
+		else
+		{
+			return false;//nav tukšs saraksts
+		}*/
+		
+		//2. īsais if-else
+		//return (howManyElements == 0) ?  true : false;
+		
+		//vel viens piemērs īsajam if un else
+		/*int aaa;
+		if(howManyElements == 0) {
+			aaa = 4 * 30;
+		}
+		else
+		{
+			aaa = 1000/4;
+		}
+		
+		aaa = (howManyElements == 0) ? 4 * 30 : 1000/4;*/
+		
+		//3.pats efektīvakais veids
 		return (howManyElements == 0);
+		
 	}
-
+	
+	
 	public boolean isFull() {
 		return (howManyElements == size);
 	}
-
+	
+	
 	private void resize() {
-		int largeSize = (howManyElements < 100) ? size * 2 : (int) (size * 1.5);
 
+		int largeSize = (howManyElements < 100)? size * 2 : (int)(size * 1.5);
+		
 		char[] largeList = new char[largeSize];
-
-		for (int i = 0; i < howManyElements; i++) {
+		
+		
+		for(int i = 0; i < howManyElements; i++) {
 			largeList[i] = list[i];
 		}
+		
 		list = largeList;
-		System.gc();
+		System.gc();//izdzēsis no RAM atmiņas mazo masīvu, jo tam vairs nav mainīgais piesaistīts
 		size = largeSize;
+		
 	}
-
+	
 	public void add(char element) {
-		if (isFull()) {
+		if(isFull()) {
 			resize();
 		}
-		list[howManyElements++] = element;
+		
+		list[howManyElements] = element;
+		howManyElements++;
+		
+		
+		
 	}
-
-	public void addAtIndex(char element, int index) {
-		if (index < 0 && index > size) {
-			throw (new Exception("Nu uH"));
+	
+	
+	public void add(char element, int index) throws Exception {
+		if(index < 0) {
+			throw (new Exception("Nevar pievienot elementu, jo index ir negatīvs"));
 		}
 	}
+
 }
